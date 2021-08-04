6f59bd521d7e392aee6d43eacc1c8e5e14d6d91b
ericpedra <45391149+ericpedra@users.noreply.github.com>
2021-08-04 14:41:50 +0700

    ssid.py

---
+++ ssid.py
@@ -0,0 +1,16 @@
+"""Module for Quotex API ssid websocket chanel."""
+
+from quotexapi.ws.chanels.base import Base
+
+
+class Ssid(Base):
+    """Class for Quotex API ssid websocket chanel."""
+    # pylint: disable=too-few-public-methods
+
+    name = "ssid"
+
+    def __call__(self, ssid):
+        """Method to send message to ssid websocket chanel.
+        :param ssid: The session identifier.
+        """
+        self.send_websocket_request(self.name, ssid)
